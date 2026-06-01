db69351ff2ce21f16f6c4efbf3df787bbcccf1fc
sitiaisahx5-glitch <sitiaisahx5-glitch@users.noreply.github.com>
2026-06-01 21:19:04 +0700

    PWA mobile version - installable, offline, receipt, settings, tax

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'kasirku-v1';
+const urlsToCache = [
+  '/kasirku-pos/',
+  '/kasirku-pos/index.html',
+  '/kasirku-pos/manifest.json'
+];
+
+// Install
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+// Fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) return response;
+        return fetch(event.request).catch(() => {
+          return caches.match('/kasirku-pos/');
+        });
+      })
+  );
+});
+
+// Activate
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(
+        names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n))
+      )
+    )
+  );
+});
